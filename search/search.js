671838c665b0eb086bffcbd39f4db308a03aeef2
shaiksalma652 <151050150+shaiksalma652@users.noreply.github.com>
2023-12-21 12:48:26 +0530

    Added few labels
    Added few labels and implementation for multiple option is failed

--- search/search.js
+++ search/search.js
@@ -2,8 +2,8 @@ function onload() {
     let todayDate = new Date();
     let ninetyDaysAgo = new Date();
     ninetyDaysAgo.setDate(todayDate.getDate() - 90);
-    console.log("todayDate = "+ todayDate);
-    console.log("todayDate = "+ ninetyDaysAgo);
+    console.log("todayDate = " + todayDate);
+    console.log("todayDate = " + ninetyDaysAgo);
     let todayDateYYYYMMDD = todayDate.toISOString().split('T')[0];
     let ninetyDaysAgoYYYYMMDD = ninetyDaysAgo.toISOString().split('T')[0];
     document.getElementById("rdf").setAttribute("min", ninetyDaysAgoYYYYMMDD);
@@ -40,5 +40,69 @@ function enableSubmit() {
     if (toDate !== "") {
         toDate.disable = false;
         toDate.setAttribute("min", rdf);
+    }
+}
+
+function multiple() {
+    const inputsWithList = document.querySelectorAll('input[list][multiple]');
+
+    if (inputsWithList && inputsWithList.length) {
+        inputsWithList.forEach(function ( /** @type {HTMLInputElement} */ input) {
+            if (input.type !== 'text' && input.type !== 'file') {
+                input.addEventListener('input', function ( /** @type {Event|InputEvent} */ event) {
+
+                    /** @type {HTMLInputElement} */
+                    const input = event.target;
+
+                    /** @type {HTMLDataListElement} */
+                    const datalist = input.list;
+
+                    /** @type {HTMLCollection} */
+                    const options = datalist.options;
+
+                    if (options && options.length) {
+                        [...options].forEach(function ( /** @type {HTMLOptionElement} */ option) {
+                            if (!option.trustedValue) {
+                                option.trustedValue = option.value;
+                                (datalist.allOptionValues = datalist.allOptionValues || new Set()).add(option.value);
+                            }
+                        });
+
+                        /** @type {Boolean} */
+                        let someValueIsSuffix = false;
+
+                        for (const option of options) {
+                            if (input.value.endsWith(option.trustedValue)) {
+                                someValueIsSuffix = true;
+                                break;
+                            }
+                        }
+                        /** @type {Array} */
+                        const parts = input.value.split(' , ').join(',').split(' ,').join(',').split(',');
+
+                        /** @type {String} */
+                        let lastPart = parts.slice(-1).pop();
+
+                        if (lastPart.startsWith(' ')) {
+                            lastPart = lastPart.trimStart();
+                        }
+                        [...options].forEach(function ( /** @type {HTMLOptionElement} */ option) {
+                            option.hidden = false;
+                            if (!someValueIsSuffix) {
+                                if (option.trustedValue.startsWith(lastPart)) {
+                                    option.value = input.value + option.trustedValue.replace(lastPart, '');
+                                } else {
+                                    option.hidden = true;
+                                }
+                            }
+                        });
+                    }
+                }, {
+                    capture: false,
+                    once: false,
+                    passive: true,
+                });
+            }
+        });
     }
 }
