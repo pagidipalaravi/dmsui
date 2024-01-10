afca0fdf2c8f00351c9f5c0dea47f24dc8501de8
shaiksalma652 <salmashaik.cloud@gmail.com>
2024-01-10 11:14:22 +0530

    dmsui-angular project

---
+++ src/app/file-upload/file-upload.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { FileUploadService } from '../service/file-upload.service';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html',
+  styleUrls: ['./file-upload.component.css']
+})
+export class FileUploadComponent implements OnInit {
+  shortLink: string = "";
+  loading: boolean = false;
+  file: File | null = null;
+
+  constructor(private fileUploadService: FileUploadService) { }
+
+  ngOnInit(): void {
+  }
+
+  onChange(event : any) {
+    this.file = event.target.files[0];
+  }
+
+  onUpload() {
+    this.loading = !this.loading;
+    console.log(this.file);
+    this.fileUploadService.upload(this.file).subscribe(
+      (event: any) => {
+        if (typeof (event) === 'object') {
+          this.shortLink = event.link;
+          this.loading = false;
+        }
+      }
+    )
+  }
+
+}
